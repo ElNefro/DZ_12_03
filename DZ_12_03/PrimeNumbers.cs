0a6ffa75194544d85fb867f8d3a4c468b139c77d
ElNefro <yanik0206@gmail.com>
2025-04-15 00:01:26 +0500

    Добавьте файлы проекта.

---
+++ DZ_12_03/PrimeNumbers.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_12_03
+{
+    public class PrimeNumbers
+    {
+        public static bool IsPrime(int num)
+        {
+            if (num < 2)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+
+        public static IEnumerable<int> GetPrimes(int limit)
+        {
+            for (int i = 2; i <= limit; i++)
+            {
+                if (IsPrime(i))
+                {
+                    yield return i;
+                }
+            }
+        }
+
+
+        public static IEnumerable<int> GetAllPrimes()
+        {
+            for (int i = 2; ; i++)
+            {
+                if (IsPrime(i))
+                {
+                    yield return i;
+                }
+            }
+        }
+
+
+        public static IEnumerable<int> SkipPrimes(int count)
+        {
+            int skipped = 0;
+            int num = 2;
+            while (skipped < count)
+            {
+                if (IsPrime(num))
+                {
+                    skipped++;
+                }
+                num++;
+            }
+
+            for (int i = num; ; i++)
+            {
+                if (IsPrime(i))
+                {
+                    yield return i;
+                }
+            }
+        }
+
+    }
+}
---
+++ DZ_12_03/Program.cs
@@ -0,0 +1,41 @@
+﻿using DZ_12_03;
+
+namespace DZ_12_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Простые числа до 60:");
+            foreach (var i in PrimeNumbers.GetPrimes(60))
+            {
+                Console.Write(i + ", ");
+            }
+
+            Console.Write("\n");
+
+
+            Console.WriteLine("Бесконечные простые числа:");
+            foreach (var i in PrimeNumbers.GetAllPrimes())
+            {
+                Console.Write(i + ", ");
+            }
+
+
+            Console.Write("\n");
+
+
+            Console.WriteLine(PrimeNumbers.IsPrime(6));
+
+
+            Console.Write("\n");
+
+
+            Console.WriteLine("Бесконечные простые числа без 10 первых простых чисел:");
+            foreach (var i in PrimeNumbers.SkipPrimes(10))
+            {
+                Console.Write(i + ", ");
+            }
+        }
+    }
+}
